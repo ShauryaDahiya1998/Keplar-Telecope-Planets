47097ae84b62cd87ace2de8ddf0bd8aaa0199337
Shaurya Dahiya <shauryadahiya@gmail.com>
2023-03-02 16:29:44 +0530

    habitable planets

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const { parse } = require('csv-parse');
+const fs = require('fs')
 
+const results = [];
 
+const isHabitablePlanet = (planet) => {
+    return planet['koi_disposition'] === 'CONFIRMED';
+}
+
+fs.createReadStream('keplar_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            results.push(data);
+        }
+        
+    })
+    .on('error', (err) => {
+        console.log(err)
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log("done");
+    })
+
+// parse(); 
